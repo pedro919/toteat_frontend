5d3c4f61b30bf738dcb6ce873272a791b86b70d2
pirioja <pirioja@uc.cl>
2022-08-01 11:50:05 -0400

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import axios from 'axios'
+
+const helloFromApi = ref('')
+
+const apiRequest = async () => {
+  const response = await axios.get('https://desolate-beyond-85011.herokuapp.com/api/hello')
+  helloFromApi.value = response.data
+}
+
+apiRequest()
+</script>
+
+<template>
+  <div class="mt-10 text-red-500">
+    {{ helloFromApi }}
+  </div>
+</template>
+
